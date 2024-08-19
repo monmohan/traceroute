8402529e6416ec837c5336d5ebb83b560f86b213
Monmohan Singh <monmohan@gmail.com>
2024-08-20 00:19:01 +0800

    Change ASN search to range, convert IP to 32 bit int

--- asn/asn.go
+++ asn/asn.go
@@ -11,13 +11,6 @@ import (
 type ASNQuery interface {
 	FindASN(ip string) (ASNData, error)
 }
-type ASNTree struct {
-	value    byte
-	children []ASNTree
-	isRoot   bool
-	isLeaf   bool
-	data     ASNData
-}
 type ASNData struct {
 	IPStart, IPEnd net.IP
 	ASNNumber      string
@@ -25,30 +18,23 @@ type ASNData struct {
 	ASName         string
 }
 
-/**
+type Reader interface {
+	ReadAll(input *csv.Reader) ASNQuery
+}
+
+type RangeReader struct {
+	FromIPs []int
+	ToIPs   []int
+	ASNData []ASNData
+}
+
+/*
+*
 Expected format of the CSV file
 IPStart\tIPEnd\tASNNumber\tCountryCode\tASName
 */
-
-func NewASNTree(input *csv.Reader) *ASNTree {
-	ASN := &ASNTree{value: 0, children: make([]ASNTree, 0), isRoot: true}
-
-	FindOrAppend := func(tree *ASNTree, value byte) *ASNTree {
-		for i, v := range tree.children {
-			if v.value == value {
-				return &tree.children[i]
-			}
-		}
-		// Value not found, append it
-		node := ASNTree{value: value, children: make([]ASNTree, 0)}
-		tree.children = append(tree.children, node)
-		return &tree.children[len(tree.children)-1]
-	}
-
-	head := 200
-	counter := 0
-	readAll := true
-	for (counter < head) || readAll {
+func (rangeReader *RangeReader) ReadAll(input *csv.Reader) {
+	for {
 		record, err := input.Read()
 		if err == io.EOF {
 			break
@@ -56,113 +42,208 @@ func NewASNTree(input *csv.Reader) *ASNTree {
 		if err != nil {
 			log.Fatal("Error reading record: ", err)
 		}
-		data := ASNData{IPStart: net.ParseIP(record[0]),
-			IPEnd:       net.ParseIP(record[1]).To4(),
-			ASNNumber:   record[2],
-			CountryCode: record[3],
-			ASName:      record[4]}
-
-		// Parse the data
-		fromIP := data.IPStart.To4()
-		child := FindOrAppend(ASN, fromIP[0])
-		for i := 1; i < len(fromIP); i++ {
-			child = FindOrAppend(child, fromIP[i])
-		}
-		child.isLeaf = true
-		child.data = data
-
-		//check if the IP in map
-		counter++
+		rangeReader.handleRecord(record)
 
 	}
-
-	//print the tree recursively
-	//PrintASNTree(*ASN, "", true)
-	return ASN
 
 }
-func PrintASNTree(node ASNTree, prefix string, isLast bool) {
-	// Print the current node
-	if node.isRoot {
-		fmt.Println("ASNTree (Root)")
-	} else {
-		if isLast {
-			fmt.Printf("%s└── ", prefix)
-			prefix += "    "
-		} else {
-			fmt.Printf("%s├── ", prefix)
-			prefix += "│   "
-		}
-		if node.isLeaf {
-			fmt.Printf("Value: %d, data %s\n", node.value, node.data)
-		} else {
-			fmt.Printf("Value: %d\n", node.value)
-		}
-
-	}
-
-	// Print children
-	for i, child := range node.children {
-		isLastChild := i == len(node.children)-1
-		PrintASNTree(child, prefix, isLastChild)
-	}
+func NewRangeReader() *RangeReader {
+	return &RangeReader{FromIPs: make([]int, 0), ToIPs: make([]int, 0), ASNData: make([]ASNData, 0)}
 }
 
-func (asn *ASNTree) FindASN(ip string) (ASNData, error) {
+func (rr *RangeReader) handleRecord(record []string) {
+	data := ASNData{IPStart: net.ParseIP(record[0]),
+		IPEnd:       net.ParseIP(record[1]).To4(),
+		ASNNumber:   record[2],
+		CountryCode: record[3],
+		ASName:      record[4]}
+
+	rr.FromIPs = append(rr.FromIPs, ToInt(data.IPStart))
+	rr.ToIPs = append(rr.ToIPs, ToInt(data.IPEnd))
+	rr.ASNData = append(rr.ASNData, data)
+
+}
+
+func ToInt(ip net.IP) int {
+	ip = ip.To4()
+	var ret int = 0
+	for i := 0; i < len(ip); i++ {
+		t := int(ip[i])
+		t <<= 8 * (3 - i)
+		ret = ret | t
+	}
+	return ret
+}
+
+func (rr *RangeReader) FindASN(ip string) (ASNData, error) {
 	//parse the IP
-	ipAddr := net.ParseIP(ip).To4()
+	ipAddr := net.ParseIP(ip)
 	if ipAddr == nil {
-		return ASNData{}, fmt.Errorf("invalid IP Address: %s", ip)
+		return ASNData{}, fmt.Errorf("invalid IP address: %s", ip)
 	}
-	return asn.findASN(ipAddr, 0)
-
+	in := ToInt(ipAddr)
+	len := len(rr.FromIPs)
+	for i := 0; i < len; i++ {
+		if in >= rr.FromIPs[i] && in <= rr.ToIPs[i] {
+			return rr.ASNData[i], nil
+		}
+	}
+	return ASNData{}, fmt.Errorf("no ASN found: %s", ip)
 }
 
-func (asn *ASNTree) findASN(ip net.IP, start int) (ASNData, error) {
-	if asn.isLeaf {
-		if isInRange(ip, asn.data.IPStart, asn.data.IPEnd) {
-			return asn.data, nil
-		}
-		return ASNData{}, fmt.Errorf("IP not found in the tree")
-	}
-	child := findChild(asn.children, ip[start])
-	if child == -1 {
-		return ASNData{}, fmt.Errorf("IP not found in the tree")
-	}
+////////////// ----This reader implementation was experiment. RangeReader is the correct implementation//////////////
+////////////// ----Keeping it commented------------------------------//////////////
 
-	return asn.children[child].findASN(ip, start+1)
-}
+/*type ASNTree struct {
+	value    byte
+	children []ASNTree
+	isRoot   bool
+	isLeaf   bool
+	data     ASNData
+}*/
 
-func isInRange(ip net.IP, start net.IP, end net.IP) bool {
-	if ip[0] < start[0] || ip[0] > end[0] {
-		return false
-	}
-	for i := 1; i < len(ip); i++ {
-		if ip[i] < start[i] || ip[i] > end[i] {
-			return false
-		}
-	}
-	return true
-}
+// The TreeReader struct represents a reader that uses a tree structure to store and query ASN data.
+// type TreeReader struct {
+// 	ASNTree *ASNTree
+// }
 
-func findChild(list []ASNTree, sval byte) int {
+// NewTreeReader creates a new instance of TreeReader with an initialized ASNTree.
+// func NewTreeReader() *TreeReader {
+// 	ASN := &ASNTree{value: 0, children: make([]ASNTree, 0), isRoot: true}
+// 	return &TreeReader{ASNTree: ASN}
+// }
 
-	for i, v := range list {
-		if i == 0 && v.value > sval {
-			return -1
-		}
-		if i == len(list)-1 && v.value < sval {
-			return i
-		}
-		if v.value == sval {
-			return i
-		}
-		if v.value > sval {
-			if i+1 < len(list) && list[i+1].value > sval {
-				return i
-			}
+// ReadAll reads all records from the input CSV reader and populates the ASNTree with the data.
+// func (treeReader *TreeReader) ReadAll(input *csv.Reader) {
+// 	for {
+// 		record, err := input.Read()
+// 		if err == io.EOF {
+// 			break
+// 		}
+// 		if err != nil {
+// 			log.Fatal("Error reading record: ", err)
+// 		}
+// 		treeReader.handleRecord(record)
+// 	}
+// }
 
-		}
-	}
-	return -1
-}
+// FindOrAppend finds a child node with the specified value in the given tree, or appends a new child node if not found.
+// func FindOrAppend(tree *ASNTree, value byte) *ASNTree {
+// 	for i, v := range tree.children {
+// 		if v.value == value {
+// 			return &tree.children[i]
+// 		}
+// 	}
+// 	// Value not found, append it
+// 	node := ASNTree{value: value, children: make([]ASNTree, 0)}
+// 	tree.children = append(tree.children, node)
+// 	return &tree.children[len(tree.children)-1]
+// }
+
+// handleRecord processes a single record from the CSV file and adds it to the ASNTree.
+// func (treeReader *TreeReader) handleRecord(record []string) {
+// 	tree := treeReader.ASNTree
+// 	data := ASNData{
+// 		IPStart:     net.ParseIP(record[0]),
+// 		IPEnd:       net.ParseIP(record[1]).To4(),
+// 		ASNNumber:   record[2],
+// 		CountryCode: record[3],
+// 		ASName:      record[4],
+// 	}
+
+// 	// Parse the data
+// 	fromIP := data.IPStart.To4()
+// 	child := FindOrAppend(tree, fromIP[0])
+// 	for i := 1; i < len(fromIP); i++ {
+// 		child = FindOrAppend(child, fromIP[i])
+// 	}
+// 	child.isLeaf = true
+// 	child.data = data
+// }
+
+// PrintASNTree prints the ASNTree structure in a tree-like format.
+// It recursively traverses the tree and prints each node along with its value and data (if available).
+// func PrintASNTree(node ASNTree, prefix string, isLast bool) {
+// 	// Print the current node
+// 	if node.isRoot {
+// 		fmt.Println("ASNTree (Root)")
+// 	} else {
+// 		if isLast {
+// 			fmt.Printf("%s└── ", prefix)
+// 			prefix += "    "
+// 		} else {
+// 			fmt.Printf("%s├── ", prefix)
+// 			prefix += "│   "
+// 		}
+// 		if node.isLeaf {
+// 			fmt.Printf("Value: %d, data %s\n", node.value, node.data)
+// 		} else {
+// 			fmt.Printf("Value: %d\n", node.value)
+// 		}
+// 	}
+
+// 	// Print children
+// 	for i, child := range node.children {
+// 		isLastChild := i == len(node.children)-1
+// 		PrintASNTree(child, prefix, isLastChild)
+// 	}
+// }
+
+// FindASN finds the ASNData associated with the given IP address in the ASNTree.
+// func (treeReader *TreeReader) FindASN(ip string) (ASNData, error) {
+// 	// Parse the IP
+// 	ipAddr := net.ParseIP(ip).To4()
+// 	if ipAddr == nil {
+// 		return ASNData{}, fmt.Errorf("invalid IP Address: %s", ip)
+// 	}
+// 	return treeReader.ASNTree.findASN(ipAddr, 0)
+// }
+
+// findASN recursively searches the ASNTree for the ASNData associated with the given IP address.
+// func (asn *ASNTree) findASN(ip net.IP, start int) (ASNData, error) {
+// 	if asn.isLeaf {
+// 		if isInRange(ip, asn.data.IPStart, asn.data.IPEnd) {
+// 			return asn.data, nil
+// 		}
+// 		return ASNData{}, fmt.Errorf("IP not found in the tree")
+// 	}
+// 	child := findChild(asn.children, ip[start])
+// 	if child == -1 {
+// 		return ASNData{}, fmt.Errorf("IP not found in the tree")
+// 	}
+// 	return asn.children[child].findASN(ip, start+1)
+// }
+
+// isInRange checks if the given IP address is within the range specified by start and end IP addresses.
+// func isInRange(ip net.IP, start net.IP, end net.IP) bool {
+// 	if ip[0] < start[0] || ip[0] > end[0] {
+// 		return false
+// 	}
+// 	for i := 1; i < len(ip); i++ {
+// 		if ip[i] < start[i] || ip[i] > end[i] {
+// 			return false
+// 		}
+// 	}
+// 	return true
+// }
+
+// findChild finds the index of the child node with the specified value in the given list of ASNTree nodes.
+// func findChild(list []ASNTree, sval byte) int {
+// 	for i, v := range list {
+// 		if i == 0 && v.value > sval {
+// 			return -1
+// 		}
+// 		if i == len(list)-1 && v.value < sval {
+// 			return i
+// 		}
+// 		if v.value == sval {
+// 			return i
+// 		}
+// 		if v.value > sval {
+// 			if i+1 < len(list) && list[i+1].value > sval {
+// 				return i
+// 			}
+// 		}
+// 	}
+// 	return -1
+// }
--- asn/asn_test.go
+++ asn/asn_test.go
@@ -2,12 +2,12 @@ package asn
 
 import (
 	"encoding/csv"
-	"fmt"
+	"net"
 	"os"
 	"testing"
 )
 
-func TestNewASN(t *testing.T) {
+func TestNewTreeReader(t *testing.T) {
 	// write test case for NewASN
 	// read the testdata/asn.csv file
 	file, err := os.Open("/Users/singhmo/CSPrimer/Networking/IP_ICMP/ip2asn-v4.tsv")
@@ -21,7 +21,8 @@ func TestNewASN(t *testing.T) {
 	reader.Comma = '\t'
 
 	// call NewASN with the csv.Reader
-	asnObj := NewASNTree(reader)
+	asnObj := NewRangeReader()
+	asnObj.ReadAll(reader)
 
 	// check if the returned object is not nil
 	if asnObj == nil {
@@ -30,7 +31,7 @@ func TestNewASN(t *testing.T) {
 
 }
 
-func TestPrintASN(t *testing.T) {
+/*func TestPrintASN(t *testing.T) {
 	tree := ASNTree{
 		value:  1,
 		isRoot: true,
@@ -42,7 +43,7 @@ func TestPrintASN(t *testing.T) {
 	}
 	PrintASNTree(tree, "", true)
 
-}
+}*/
 
 func TestFindASN(t *testing.T) {
 	// write test case for FindASN
@@ -58,7 +59,8 @@ func TestFindASN(t *testing.T) {
 	reader.Comma = '\t'
 
 	// call NewASN with the csv.Reader
-	asnObj := NewASNTree(reader)
+	asnObj := NewRangeReader()
+	asnObj.ReadAll(reader)
 
 	// check if the returned object is not nil
 	if asnObj == nil {
@@ -67,11 +69,100 @@ func TestFindASN(t *testing.T) {
 
 	// call FindASN with a valid IP address
 	// check if the returned ASNData is not nil
-	ip := "1.5.140.0"
-	asnData, err := asnObj.FindASN(ip)
-	if err != nil {
-		t.Fatalf("failed to find ASN: %v", err)
+	//add test for multiple IPs
+
+	tests := []struct {
+		ip   string
+		want ASNData
+	}{
+		{
+
+			ip: "1.5.140.0",
+			want: ASNData{
+				IPStart:     net.ParseIP("1.5.0.0"),
+				IPEnd:       net.ParseIP("1.5.255.255"),
+				ASNNumber:   "4725",
+				CountryCode: "JP",
+				ASName:      "ODN SoftBank Corp.",
+			},
+		},
+		{
+			ip: "203.118.7.77",
+			want: ASNData{
+				IPStart:     net.ParseIP("203.117.254.0"),
+				IPEnd:       net.ParseIP("203.118.10.255"),
+				ASNNumber:   "4657",
+				CountryCode: "SG",
+				ASName:      "STARHUB-INTERNET StarHub Ltd",
+			},
+		},
+		{
+
+			ip: "207.45.219.137",
+			want: ASNData{
+				IPStart:     net.ParseIP("207.45.192.0"),
+				IPEnd:       net.ParseIP("207.45.223.255"),
+				ASNNumber:   "6453",
+				CountryCode: "US",
+				ASName:      "AS6453",
+			},
+		},
+		{
+			ip: "183.90.44.189",
+			want: ASNData{
+				IPStart:     net.ParseIP("183.90.40.0"),
+				IPEnd:       net.ParseIP("183.90.127.255"),
+				ASNNumber:   "55430",
+				CountryCode: "SG",
+				ASName:      "STARHUB-NGNBN Starhub Ltd",
+			},
+		},
+		{
+			ip: "99.83.65.110",
+			want: ASNData{
+				IPStart:     net.ParseIP("99.83.64.0"),
+				IPEnd:       net.ParseIP("99.83.71.255"),
+				ASNNumber:   "0",
+				CountryCode: "None",
+				ASName:      "Not routed",
+			},
+		},
+		{
+			ip: "108.170.234.57",
+			want: ASNData{
+				IPStart:     net.ParseIP("108.170.224.0"),
+				IPEnd:       net.ParseIP("108.170.255.255"),
+				ASNNumber:   "15169",
+				CountryCode: "US",
+				ASName:      "GOOGLE",
+			},
+		},
 	}
-	fmt.Println(asnData)
+	for _, test := range tests {
+		t.Run(test.ip, func(t *testing.T) {
+			got, err := asnObj.FindASN(test.ip)
+			if err != nil {
+				t.Fatalf("failed to find ASN: %v", err)
+			}
+			if got.ASNNumber != test.want.ASNNumber && got.ASName != test.want.ASName && got.CountryCode != test.want.CountryCode {
+				t.Errorf("got: %v, want: %v", got, test.want)
+			}
+		})
+	}
+
+	/*--Just printing the ASNData for the IPs--*/
+	/*ips := []string{"1.5.140.0", "203.118.7.77", "207.45.219.137",
+		"216.6.87.227", "183.90.44.189",
+		"142.251.230.137", "99.83.65.110", "108.170.234.57", "180.87.106.0"}
+
+	for _, ip := range ips {
+		fmt.Println("Finding ASN for IP:", ip)
+
+		asnData, err := asnObj.FindASN(ip)
+		if err != nil {
+			t.Fatalf("failed to find ASN: %v", err)
+		}
+		fmt.Println(asnData)
+	}*/
 
 }
